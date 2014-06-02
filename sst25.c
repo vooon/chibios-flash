8e532ede469f1bd4e7318765732e8afc12c4cc6d
Vladimir Ermakov <vooon341@gmail.com>
2014-06-02 12:59:30 +0400

    Add license headers.

--- sst25.c
+++ sst25.c
@@ -1,6 +1,27 @@
-
-// TODO: add "based on 25xx.c from ChibiOS-EEPROM"
-// TODO: check support for non sst25 devices
+/**
+ * @file       sst25.c
+ * @brief      FLASH25 SST25 driver
+ * @author     Vladimir Ermakov Copyright (C) 2014.
+ * @see        The GNU Public License (GPL) Version 3
+ *
+ * Based on 25xx.c from ChibiOS-EEPROM written by Timon Wong
+ * And sst25.c from NuttX written by Gregory Nutt <gnutt@nuttx.org>
+ */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 
 #include "sst25.h"
 
